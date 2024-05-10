410dc5063b2216a1e11661c1ac2f67f416c550c3
Bastien Rigaud <contact.teyz+github@gmail.com>
2024-05-10 23:01:51 +0200

    fix: clean seeds && cron

--- src/cmd/main.go
+++ src/cmd/main.go
@@ -10,7 +10,6 @@ import (
 	"github.com/rs/zerolog/log"
 
 	"github.com/teyz/songify-svc/internal/config"
-	"github.com/teyz/songify-svc/internal/cron"
 	database_postgres "github.com/teyz/songify-svc/internal/database/postgres"
 	handlers_http "github.com/teyz/songify-svc/internal/handlers/http"
 	pkg_config "github.com/teyz/songify-svc/internal/pkg/config"
@@ -44,14 +43,6 @@ func main() {
 		log.Fatal().Err(err).
 			Msg("main: unable to create user store service")
 	}
-
-	newCron, err := cron.NewCron(ctx, service)
-	if err != nil {
-		log.Fatal().Err(err).
-			Msg("main: unable to create cron service")
-	}
-
-	newCron.CreateGameCron(ctx)
 
 	// create http server
 	httpServer, err := handlers_http.NewServer(ctx, cfg.HTTPServerConfig, service)
